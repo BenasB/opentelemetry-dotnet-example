99a29cf6c78eb4ddbe36daccb2dd483b8cd14679
BenasB <29711974+BenasB@users.noreply.github.com>
2023-03-12 00:08:53 +0200

    Add docker support and local otel config

--- Roomr.Reservations/Program.cs
+++ Roomr.Reservations/Program.cs
@@ -1,7 +1,19 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var serviceName = typeof(Program).Assembly.GetName().Name ?? "Unknown service";
+builder.Services.AddOpenTelemetry().WithTracing(builder =>
+{
+    builder
+        .AddConsoleExporter()
+        .ConfigureResource(builder => builder.AddService(serviceName))
+        .AddAspNetCoreInstrumentation();
+});
 
 var app = builder.Build();
 
