59a708b789886681559bad0da661022596692487
BenasB <29711974+BenasB@users.noreply.github.com>
2023-03-21 15:16:44 +0200

    Add healthcheck to db

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Roomr
 
 Roomr (pronounced the same as roomer) is a mock room reservation application built as an exercise to learn [OpenTelemetry](https://opentelemetry.io/) in .NET
+
+<img width="986" alt="image" src="https://user-images.githubusercontent.com/29711974/226617075-55406959-a450-4081-8307-5907b5a29fc2.png">
--- Roomr.Auth/Program.cs
+++ Roomr.Auth/Program.cs
@@ -23,7 +23,6 @@ builder.Services.AddOpenTelemetry().WithTracing(builder =>
 });
 
 var usersDatabaseConnectionString = builder.Configuration.GetConnectionString("UsersDatabase");
-Console.WriteLine($"Trying to use database connection string: '{usersDatabaseConnectionString}'");
 builder.Services.AddDbContext<AuthDbContext>(options =>
     options.UseMySql(usersDatabaseConnectionString, ServerVersion.AutoDetect(usersDatabaseConnectionString))
 );
